f2607b65934d221731c94fde1382761bc79ce6e6
luismdsleite <luismdsleite@gmail.com>
2021-11-29 14:49:03 +0000

    fixed return

--- src/app.c
+++ src/app.c
@@ -261,7 +261,7 @@ int main(int argc, char const *argv[])
         printf("Saved to %s file with %zu bytes\n", file_name, file_size);
         fclose(file_to_write);
         free(file_name);
-        return length;
+        return 0;
     }
 
     return 0;
