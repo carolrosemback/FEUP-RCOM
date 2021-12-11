867a34a98231583d59de5d69cb3fd55c1fe9cc74
luismdsleite <luismdsleite@gmail.com>
2021-12-11 18:44:59 +0000

    ssh

--- src/datalink.h
+++ src/datalink.h
@@ -38,7 +38,7 @@
 // Commands sent by the receiver and replies sent by transmitter
 #define AF_REC 0x01
 
-#define BAUDRATE B230400
+#define BAUDRATE B460800
 
 // Escape bytes
 #define ESC_BYTE1 0x7E
