b7e0beffab67921b84cc3ede79a030e9636ca59b
luismdsleite <luismdsleite@gmail.com>
2021-12-12 00:36:42 +0000

    ssh

--- src/statistics.h
+++ src/statistics.h
@@ -11,7 +11,7 @@
 
 #define FER_HEADER 0 /* error % on the header */
 #define FER_DATA 0   /* error % on the data */
-#define EXTRA_T_PROP 10000 // Extra propagation time that will be added using usleep
+#define EXTRA_T_PROP 50000 // Extra propagation time that will be added using usleep
 
 
 uint64_t time_passed(struct timespec, struct timespec);
